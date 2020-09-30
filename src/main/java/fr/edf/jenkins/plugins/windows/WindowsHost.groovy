8fd652627e88eff2f4e0a91ab9d6940c0a2f451a
Chris Bahonda <chrisbahonda@gmail.com>
2020-09-30 18:17:03 +0200

    Pom and WindowsHost update plus creation of resources to enable user interface

--- src/main/java/fr/edf/jenkins/plugins/windows/WindowsHost.groovy
+++ src/main/java/fr/edf/jenkins/plugins/windows/WindowsHost.groovy
@@ -1,71 +1,188 @@
 package fr.edf.jenkins.plugins.windows
 
+import org.apache.http.client.config.AuthSchemes
 import org.kohsuke.stapler.DataBoundConstructor
 import org.kohsuke.stapler.DataBoundSetter
+import org.kohsuke.stapler.QueryParameter
+import org.kohsuke.stapler.verb.POST
+
+import com.cloudbees.plugins.credentials.common.StandardListBoxModel
 
 import hudson.Extension
 import hudson.model.Describable
 import hudson.model.Descriptor
+import hudson.util.ListBoxModel
 import jenkins.model.Jenkins
 
 class WindowsHost implements Describable<WindowsHost> {
 
-    String name
     String host
+    String credentialsId
     Integer port
     String authenticationScheme
+    Integer maxUsers
+    Boolean disable
+    Integer connectionTimeout
+    Integer agentConnectionTimeout
+    Integer maxTries
+    String labelString
     Boolean useHttps
 
     @DataBoundConstructor
-    WindowsHost(String name) {
-        this.name = name
+    WindowsHost(String host, String credentialsId, Integer port, String authenticationScheme, Integer maxUsers,
+    Boolean disable,Integer connectionTimeout, Integer agentConnectionTimeout, Integer maxTries, String labelString, Boolean useHttps) {
+        this.host = host
+        this.credentialsId = credentialsId
+        this.port = port
+        this.authenticationScheme = authenticationScheme
+        this.maxUsers = maxUsers
+        this.disable = disable
+        this.connectionTimeout = connectionTimeout
+        this.agentConnectionTimeout = agentConnectionTimeout
+        this.maxTries = maxTries
+        this.labelString = labelString
+        this.useHttps = useHttps
     }
 
-    String getName() {
-        return name
-    }
+
 
     String getHost() {
         return host
     }
+
 
     @DataBoundSetter
     void setHost(String host) {
         this.host = host
     }
 
+
+
+    String getCredentialsId() {
+        return credentialsId
+    }
+
+
+    @DataBoundSetter
+    void setCredentialsId(String credentialsId) {
+        this.credentialsId = credentialsId
+    }
+
+
+
     Integer getPort() {
         return port
     }
+
 
     @DataBoundSetter
     void setPort(Integer port) {
         this.port = port
     }
 
+
+
     String getAuthenticationScheme() {
         return authenticationScheme
     }
+
 
     @DataBoundSetter
     void setAuthenticationScheme(String authenticationScheme) {
         this.authenticationScheme = authenticationScheme
     }
 
+
+
+    Integer getMaxUsers() {
+        return maxUsers
+    }
+
+
+    @DataBoundSetter
+    void setMaxUsers(Integer maxUsers) {
+        this.maxUsers = maxUsers
+    }
+
+    
+
+     Boolean getDisable() {
+        return disable
+    }
+
+
+    @DataBoundSetter
+     void setDisable(Boolean disable) {
+        this.disable = disable
+    }
+
+
+
+    Integer getConnectionTimeout() {
+        return connectionTimeout
+    }
+
+
+    @DataBoundSetter
+    void setConnectionTimeout(Integer connectionTimeout) {
+        this.connectionTimeout = connectionTimeout
+    }
+
+
+
+    Integer getAgentConnectionTimeout() {
+        return agentConnectionTimeout
+    }
+
+
+    @DataBoundSetter
+    void setAgentConnectionTimeout(Integer agentConnectionTimeout) {
+        this.agentConnectionTimeout = agentConnectionTimeout
+    }
+
+    
+    
+     Integer getMaxTries() {
+        return maxTries
+    }
+
+
+    @DataBoundSetter
+     void setMaxTries(Integer maxTries) {
+        this.maxTries = maxTries
+    }
+
+
+
+    String getLabelString() {
+        return labelString
+    }
+
+
+    @DataBoundSetter
+    void setLabelString(String labelString) {
+        this.labelString = labelString
+    }
+
+
+
     Boolean getUseHttps() {
         return useHttps
     }
+
 
     @DataBoundSetter
     void setUseHttps(Boolean useHttps) {
         this.useHttps = useHttps
     }
-    
+
+
+
     @Override
     Descriptor<WindowsHost> getDescriptor() {
         return Jenkins.get().getDescriptorOrDie(this.getClass())
     }
-    
+
     @Extension
     static class DescriptorImpl extends Descriptor<WindowsHost> {
         /**
@@ -74,6 +191,14 @@ class WindowsHost implements Describable<WindowsHost> {
         @Override
         String getDisplayName() {
             return "Windows Host"
+        }
+        @POST
+        ListBoxModel doFillAuthenticationSchemeItems(@QueryParameter String authenticationScheme) {
+            ListBoxModel result = new ListBoxModel()
+            [AuthSchemes.NTLM, AuthSchemes.BASIC, AuthSchemes.KERBEROS].each { 
+                result.add(it,it)
+            }
+            return result
         }
     }
 }
---
+++ src/main/resources/fr/edf/jenkins/plugins/windows/WindowsHost/config.groovy
@@ -0,0 +1,50 @@
+import org.apache.http.client.config.AuthSchemes
+
+def f = namespace(lib.FormTagLib)
+def c = namespace(lib.CredentialsTagLib)
+
+f.entry(title: 'Host', field:'host') {
+    f.textbox(clazz: 'required', checkMethod: 'post')
+}
+
+f.entry(title: 'Disable', field:'disable') {
+    f.checkbox()
+}
+
+f.advanced(title:'Windows Host Details') {
+    
+        f.entry(title:'Label', field:'labelString') {
+            f.textbox()
+        }
+    
+        f.entry(title: 'Maximum Tries', field: 'maxTries') {
+            f.number(clazz: 'required', min: 1, default: 5)
+        }
+    
+        f.entry(title: 'Port Number', field: 'port') {
+            f.number(clazz: 'required', default: 5895, min: 1)
+        }
+        
+        f.entry(title: 'Authentication Scheme', field: 'authenticationScheme'){
+            f.select(default: AuthSchemes.NTLM)
+        }
+    
+    
+        f.entry(title: 'Maximum Users', field: 'maxUsers') {
+            f.number(clazz: 'required', min: 1)
+        }
+    
+    
+        f.block() {
+            f.validateButton(
+                    title: 'Test Connection',
+                    progress: 'Testing...',
+                    method: 'verifyConnection',
+                    with: 'host,port'
+                    )
+        }
+    
+    
+    }
+    
+

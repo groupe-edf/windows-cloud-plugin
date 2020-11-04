a47faa3c3b2a9441c6ff0c2d6dad2af647f43161
Chris Bahonda <chrisbahonda@gmail.com>
2020-11-04 19:23:38 +0100

    WindowsComputerConnector class was created

---
+++ src/main/java/fr/edf/jenkins/plugins/windows/connector/WindowsComputerConnector.groovy
@@ -0,0 +1,18 @@
+package fr.edf.jenkins.plugins.windows.connector
+
+import fr.edf.jenkins.plugins.windows.WindowsHost
+import fr.edf.jenkins.plugins.windows.WindowsUser
+import hudson.model.AbstractDescribableImpl
+import hudson.slaves.ComputerLauncher
+abstract class WindowsComputerConnector extends AbstractDescribableImpl<WindowsComputerConnector>{
+    
+    /**
+     * Build and return the Launcher for a given connector
+     * @param host
+     * @param user
+     * @return Computer Launcher 
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    protected abstract ComputerLauncher createLauncher(WindowsHost host, WindowsUser user) throws IOException, InterruptedException
+}

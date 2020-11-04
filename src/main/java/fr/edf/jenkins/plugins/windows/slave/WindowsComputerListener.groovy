9ac28cfe5c61c14dec8ca924fa23c14ca09e068f
Chris Bahonda <chrisbahonda@gmail.com>
2020-11-04 14:23:04 +0100

    WindowsComputerListener class added

---
+++ src/main/java/fr/edf/jenkins/plugins/windows/slave/WindowsComputerListener.groovy
@@ -0,0 +1,48 @@
+package fr.edf.jenkins.plugins.windows.slave
+
+import java.util.logging.Level
+import java.util.logging.Logger
+
+import hudson.model.Computer
+import hudson.model.TaskListener
+import hudson.slaves.ComputerListener
+import hudson.slaves.OfflineCause
+
+class WindowsComputerListener extends ComputerListener{
+
+    private static final Logger LOGGER = Logger.getLogger(WindowsComputerListener.class.name)
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    void onLaunchFailure(Computer c, TaskListener taskListener) throws IOException, InterruptedException {
+        if(c instanceof WindowsComputer) {
+            WindowsComputer windowsComputer = (WindowsComputer) c
+            LOGGER.log(Level.WARNING, "Windows Agent {0} failed to launch and will be removed", windowsComputer.getName())
+            windowsComputer.getNode().terminate()
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    void onOnline(Computer c, TaskListener listener) {
+        if(c instanceof WindowsComputer) {
+            WindowsComputer windowsComputer = (WindowsComputer) c
+            LOGGER.log(Level.FINE, "Windows Agent {} is now online", windowsComputer.name)
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    void onOffline(Computer c, OfflineCause cause) {
+        if(c instanceof WindowsComputer) {
+            WindowsComputer windowsComputer = (WindowsComputer) c
+            LOGGER.log(Level.FINE, "Windows Agent {} is now offline", windowsComputer.name)
+        }
+    }
+}

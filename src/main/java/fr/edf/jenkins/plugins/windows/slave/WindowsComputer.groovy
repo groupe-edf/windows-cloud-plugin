6e6129073747598805677682968e394dd318b051
Chris Bahonda <chrisbahonda@gmail.com>
2020-11-04 12:11:03 +0100

    WindowsComputer class created

---
+++ src/main/java/fr/edf/jenkins/plugins/windows/slave/WindowsComputer.groovy
@@ -0,0 +1,95 @@
+package fr.edf.jenkins.plugins.windows.slave
+
+import java.util.logging.Level
+import java.util.logging.Logger
+
+import javax.annotation.CheckForNull
+
+import com.google.common.base.Objects
+
+import fr.edf.jenkins.plugins.windows.WindowsCloud
+import fr.edf.jenkins.plugins.windows.WindowsHost
+import hudson.model.Executor
+import hudson.model.Queue
+import hudson.model.Queue.Task
+import hudson.slaves.AbstractCloudComputer
+
+class WindowsComputer extends AbstractCloudComputer<WindowsSlave>{
+
+    private static final Logger LOGGER = Logger.getLogger(WindowsComputer.class.name)
+
+    WindowsComputer(WindowsSlave node){
+        super(node)
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @CheckForNull
+    @Override
+    WindowsSlave getNode() {
+        return (WindowsSlave) super.getNode()
+    }
+
+    @CheckForNull
+    WindowsCloud getCloud() {
+        final WindowsSlave node = getNode()
+        return node == null ?: node.getCloud()
+    }
+
+    @CheckForNull
+    String getUserId() {
+        final WindowsSlave node = getNode()
+        return node == null ?: node.getUserId()
+    }
+
+    @CheckForNull
+    String getCloudId() {
+        final WindowsSlave node = getNode()
+        return node == null ?: node.getCloudId()
+    }
+
+    @CheckForNull
+    WindowsHost getWindowsHost() {
+        final WindowsSlave node = getNode()
+        return node == null ?: node.getHost()
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    void taskAccepted(Executor executor, Queue.Task task) {
+        super.taskAccepted(executor, task)
+        Queue.Executable exec = executor.getCurrentExecutable()
+        LOGGER.log(Level.FINE, "Computer {0} accepted task {1}", this, exec)
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    void taskCompleted(Executor executor, Task task, long durationMS) {
+        Queue.Executable exec = executor.getCurrentExecutable()
+        LOGGER.log(Level.FINE, "Computer {0} completed task {1}", this, exec)
+        super.taskCompleted(executor, task, durationMS)
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    void taskCompletedWithProblems(Executor executor, Task task, long durationMS, Throwable problems) {
+        Queue.Executable exec = executor.getCurrentExecutable()
+        LOGGER.log(Level.FINE, "Computer {0} completed task {1} with problems", this, exec)
+        super.taskCompletedWithProblems(executor, task, durationMS, problems)
+    }
+
+    @Override
+    String toString() {
+        return Objects.toStringHelper(this)
+                .add("name", super.getName())
+                .add("slave", getNode())
+                .toString()
+    }
+}

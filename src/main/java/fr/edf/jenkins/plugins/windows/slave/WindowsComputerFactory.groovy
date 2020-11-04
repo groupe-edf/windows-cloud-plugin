9e36c6e5b564741a5546a0c71ce2865a810b9c55
Chris Bahonda <chrisbahonda@gmail.com>
2020-11-04 10:09:42 +0100

    WindowsComputerFactory class is created

---
+++ src/main/java/fr/edf/jenkins/plugins/windows/slave/WindowsComputerFactory.groovy
@@ -0,0 +1,45 @@
+package fr.edf.jenkins.plugins.windows.slave
+
+import hudson.ExtensionList
+import hudson.ExtensionPoint
+
+/**
+ * A factory of {@link WindowsComputer} instances.
+ * @author CHRIS BAHONDA
+ *
+ */
+abstract class WindowsComputerFactory implements ExtensionPoint{
+    
+    /**
+     * return all registered implementations of {@link WindowsComputerFactory}
+     * @return all registered implementations of {@link WindowsComputerFactory}
+     */
+    static ExtensionList <WindowsComputerFactory> all(){
+        return ExtensionList.lookup(WindowsComputerFactory.class)
+    }
+    
+    /**
+     * Creates a new instance of {@link WindowsComputer}
+     * @param slave
+     * @return
+     */
+    abstract WindowsComputer newInstance(WindowsSlave slave)
+    
+    /**
+     * Returns a new instance of {@link WindowsComputer}
+     * @param slave
+     * @return a new instance of {@link WindowsComputer}
+     */
+    static WindowsComputer createInstance(WindowsSlave slave) {
+        for (WindowsComputerFactory factory : all()) {
+            WindowsComputer computer = factory.newInstance(slave) 
+            if(computer != null) {
+                return computer
+            }
+        }
+        return new WindowsComputer(slave)
+    }
+    
+    
+        
+}

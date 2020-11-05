2c0026a823e707d2bb4c2be43cfde4375a07e913
Mathieu Delrocq <delrocq.mathieu@gmail.com>
2020-11-05 15:11:59 +0100

    fix exception message

--- src/main/java/fr/edf/jenkins/plugins/windows/winrm/client/WinRMException.groovy
+++ src/main/java/fr/edf/jenkins/plugins/windows/winrm/client/WinRMException.groovy
@@ -23,8 +23,8 @@ package fr.edf.jenkins.plugins.windows.winrm.client
  */
 class WinRMException extends Exception {
     
-    public static final String FORMATTED_MESSAGE = "Error during %s command, protocol : %s, code : %s, description : %"
-    
+    public static final String FORMATTED_MESSAGE = "Error during %s command, protocol : %s, code : %s, description : %s"
+
     private WinRMException() {
         // Hide default constructor
     }

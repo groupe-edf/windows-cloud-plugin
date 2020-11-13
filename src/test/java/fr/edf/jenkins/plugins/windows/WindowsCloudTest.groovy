06e912e4507a195c0c6a112e8f00ed7d7ebad3e0
Chris Bahonda <chrisbahonda@gmail.com>
2020-11-13 16:43:04 +0100

    New test method for WindowsCloud

--- src/test/java/fr/edf/jenkins/plugins/windows/WindowsCloudTest.groovy
+++ src/test/java/fr/edf/jenkins/plugins/windows/WindowsCloudTest.groovy
@@ -5,6 +5,8 @@ import org.junit.Rule
 import org.jvnet.hudson.test.JenkinsRule
 
 import fr.edf.jenkins.plugins.windows.pojos.WindowsPojoBuilder
+import hudson.model.FreeStyleProject
+import hudson.model.Label
 import spock.lang.Specification
 
 class WindowsCloudTest extends Specification{
@@ -13,17 +15,33 @@ class WindowsCloudTest extends Specification{
     JenkinsRule rule = new JenkinsRule()
     
     def "create cloud"(){
+        
         given:
-        WindowsCloud cloud = WindowsPojoBuilder.buildWindowsCloud(WindowsPojoBuilder.buildWindowsHost())
+        WindowsCloud cloud = WindowsPojoBuilder.buildWindowsCloud(WindowsPojoBuilder.buildWindowsHost(), WindowsPojoBuilder.buildConnector(rule))
         
         when:
         rule.jenkins.clouds.add(cloud)
         
         then:
         notThrown Exception
-        cloud == rule.jenkins.getCloud("cloudTest")
+        cloud == rule.jenkins.getCloud("testCloud")
+        rule.jenkins.clouds.size()==1
     }
     
-
+    def "call provision method"(){
+        
+        given:
+        WindowsCloud cloud = WindowsPojoBuilder.buildWindowsCloud(WindowsPojoBuilder.buildWindowsHost(), WindowsPojoBuilder.buildConnector(rule))
+        rule.jenkins.getCloud("testCloud")
+        FreeStyleProject project = rule.createFreeStyleProject("test")
+        project.setAssignedLabel(Label.parse("testLabel").getAt(0))
+        
+        when:
+        Boolean isBuilt = project.scheduleBuild2(1)
+        
+        then:
+        notThrown Exception
+        isBuilt==true
+    }
 
 }

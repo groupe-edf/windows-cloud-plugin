aea78cbddef3ebbacd1924cf7a17cbe51c294db3
Chris Bahonda <chrisbahonda@gmail.com>
2020-11-13 16:36:09 +0100

    WindowsCloud methods filled

--- src/main/java/fr/edf/jenkins/plugins/windows/WindowsCloud.groovy
+++ src/main/java/fr/edf/jenkins/plugins/windows/WindowsCloud.groovy
@@ -1,7 +1,17 @@
 package fr.edf.jenkins.plugins.windows
 
+import java.util.logging.Level
+import java.util.logging.Logger
+
+import org.antlr.v4.runtime.misc.Nullable
+import org.apache.commons.collections.CollectionUtils
 import org.kohsuke.stapler.DataBoundConstructor
 
+import fr.edf.jenkins.plugins.windows.connector.WindowsComputerConnector
+import fr.edf.jenkins.plugins.windows.planned.PlannedNodeBuilderFactory
+import fr.edf.jenkins.plugins.windows.provisioning.InProvisioning
+import fr.edf.jenkins.plugins.windows.winrm.WinRMCommand
+import fr.edf.jenkins.plugins.windows.winrm.WinRMCommandException
 import hudson.Extension
 import hudson.model.Descriptor
 import hudson.model.Label
@@ -14,16 +24,45 @@ import hudson.slaves.NodeProvisioner.PlannedNode
  */
 class WindowsCloud extends /*AbstractCloudImpl*/ Cloud {
 
-    List<WindowsHost> windowsHosts
+    private static final Logger LOGGER = Logger.getLogger(WindowsCloud.class.name)
+
+    List<WindowsHost> windowsHosts = new ArrayList()
+    WindowsComputerConnector connector
+    Integer idleMinutes
 
     @DataBoundConstructor
-    WindowsCloud(String name, /*String instanceCapStr,*/ List<WindowsHost> windowsHosts) {
-        super(name/*, instanceCapStr*/)
+    WindowsCloud(String name, List<WindowsHost> windowsHosts, WindowsComputerConnector connector, Integer idleMinutes) {
+        super(name)
         this.windowsHosts = windowsHosts
+        this.connector = connector
+        this.idleMinutes = idleMinutes
     }
 
     List<WindowsHost> getWindowsHosts() {
         return windowsHosts
+    }
+
+    @Nullable
+    static getWindowsCloud() {
+        return all().get(WindowsCloud)
+    }
+
+    /**
+     * Gets all MacHost available for the given label
+     * @param label
+     * @return All MacHosts of this cloud not disabled and matching the given label
+     */
+    List<WindowsHost> getWindowsHosts(Label label) {
+        try {
+            return windowsHosts.findAll {
+                !it.disable && label.matches((Collection) it.getLabelSet())
+            }
+        } catch(Exception e) {
+            String message = String.format("An error occured when trying to find hosts with label %s", label.toString())
+            LOGGER.log(Level.WARNING, message)
+            LOGGER.log(Level.FINEST, "Exception : ", e)
+            return Collections.emptyList()
+        }
     }
 
     /**
@@ -31,7 +70,27 @@ class WindowsCloud extends /*AbstractCloudImpl*/ Cloud {
      */
     @Override
     synchronized Collection<PlannedNode> provision(Label label, int excessWorkload) {
-        return Collections.emptyList()
+        try {
+            List<WindowsHost> labelWindowsHosts = getWindowsHosts(label)
+            if(CollectionUtils.isEmpty(labelWindowsHosts)) {
+                LOGGER.log(Level.WARNING, "No host is configured for the label {0}", label.toString())
+                return Collections.emptyList()
+            }
+            final List<PlannedNode> r = new ArrayList<>()
+            Set<String> allInProvisioning = InProvisioning.getAllInProvisioning(label)
+            LOGGER.log(Level.FINE, "In provisioning : {0}", allInProvisioning.size())
+            int toBeProvisioned = Math.max(0, excessWorkload - allInProvisioning.size())
+            LOGGER.log(Level.INFO, "Excess workload after pending Windows agents: {0}", toBeProvisioned)
+            if(toBeProvisioned > 0) {
+                WindowsHost windowsHost = chooseWindowsHost(labelWindowsHosts)
+                r.add(PlannedNodeBuilderFactory.createInstance().cloud(this).host(windowsHost).build())
+            }
+            return r
+        } catch (Exception e) {
+            LOGGER.log(Level.WARNING, e.getMessage())
+            LOGGER.log(Level.FINEST, "Exception : ", e)
+            return Collections.emptyList()
+        }
     }
 
     /**
@@ -39,7 +98,44 @@ class WindowsCloud extends /*AbstractCloudImpl*/ Cloud {
      */
     @Override
     boolean canProvision(Label label) {
-        return false
+        boolean canProvision = windowsHosts.find {!it.disable} != null
+        if(!canProvision) {
+            LOGGER.log(Level.WARNING, "The Windows Cloud {0} is disabled", this.name)
+        }
+        return canProvision
+    }
+
+    /**
+     * Returns an available Windows host
+     * The host must not be disabled and must be able to create users
+     * @param labelWindowsHosts
+     * @return WindowsHost
+     * @throws Exception
+     */
+    private WindowsHost chooseWindowsHost(List<WindowsHost> labelWindowsHosts) throws Exception {
+        WindowsHost hostChoosen = labelWindowsHosts.find {
+            if(it.disable) {
+                return false
+            }
+            int nbTries = 0
+            while(true) {
+                try {
+                    int existingUsers = WinRMCommand.listUsers(it).size()
+                    return existingUsers < it.maxUsers
+                } catch(WinRMCommandException e) {
+                    nbTries ++
+                    if(nbTries < it.maxTries) {
+                        continue
+                    } else {
+                        LOGGER.log(Level.INFO, "Disabling Windows Host {0}", it.host)
+                        it.disable = true
+                        return false
+                    }
+                }
+            }
+        }
+        if(null == hostChoosen) throw new Exception("Unable to find a Windows host available")
+        return hostChoosen
     }
 
     @Extension

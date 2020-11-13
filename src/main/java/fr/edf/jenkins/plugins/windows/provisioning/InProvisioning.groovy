2a85939e56121d545f8a248de8d9b6e96a4e25bd
Chris Bahonda <chrisbahonda@gmail.com>
2020-11-13 17:19:41 +0100

    Provision for nodes

---
+++ src/main/java/fr/edf/jenkins/plugins/windows/provisioning/InProvisioning.groovy
@@ -0,0 +1,34 @@
+package fr.edf.jenkins.plugins.windows.provisioning
+
+import javax.annotation.CheckForNull
+import javax.annotation.Nonnull
+
+import hudson.ExtensionList
+import hudson.ExtensionPoint
+import hudson.model.Label
+
+abstract class InProvisioning implements ExtensionPoint{
+
+    /**
+     * Returns the agents names in provisioning according to all implementations of this extension point for the given label
+     * @param label the {@link Label} being checked
+     * @return the agents names in provisioning according to all implementations of this extension point for the given label
+     */
+    @Nonnull
+    static Set<String> getAllInProvisioning(@CheckForNull Label label) {
+        return all().collect{ it.getInProvisioning(label) }.collectMany([] as HashSet){ it }
+    }
+
+
+    static ExtensionList<InProvisioning> all() {
+        return ExtensionList.lookup(InProvisioning)
+    }
+
+    /**
+     * Returns the agents in provisioning for the current label
+     * @param label the label being checked
+     * @return The agents names in provisioning for the current label
+     */
+    @Nonnull
+    abstract Set<String> getInProvisioning(Label label)
+}
---
+++ src/main/java/fr/edf/jenkins/plugins/windows/provisioning/WindowsInProvisioning.groovy
@@ -0,0 +1,36 @@
+package fr.edf.jenkins.plugins.windows.provisioning
+
+import javax.annotation.CheckForNull
+
+import fr.edf.jenkins.plugins.windows.slave.WindowsSlave
+import hudson.Extension
+import hudson.model.Computer
+import hudson.model.Label
+import hudson.model.Node
+
+@Extension
+class WindowsInProvisioning extends InProvisioning{
+    
+    private static boolean isNotAcceptingTasks(Node n) {
+        Computer computer = n.toComputer()
+        return computer != null && (computer.isLaunchSupported()
+                || !computer.isAcceptingTasks() // Launcher has not been called yet
+                || !n.isAcceptingTasks()) // node is not ready yet
+    }
+
+    @Override
+    Set<String> getInProvisioning(@CheckForNull Label label) {
+        if (label != null) {
+            return label.getNodes().stream()
+                    .filter { node ->
+                        node instanceof WindowsSlave
+                    }
+                    .filter { node ->
+                        WindowsInProvisioning.isNotAcceptingTasks(node)
+                    }
+                    .collect([] as HashSet){it.name}
+        } else {
+            return Collections.emptySet();
+        }
+    }
+}

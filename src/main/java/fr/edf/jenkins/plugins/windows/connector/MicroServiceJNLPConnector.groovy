299d07044af81c102f01fb2e4fe5a0a808f30a7e
Mathieu Delrocq <delrocq.mathieu@gmail.com>
2021-04-29 18:24:00 +0200

    ui config of microservice connection

--- src/main/java/fr/edf/jenkins/plugins/windows/connector/MicroServiceJNLPConnector.groovy
+++ src/main/java/fr/edf/jenkins/plugins/windows/connector/MicroServiceJNLPConnector.groovy
@@ -1,38 +1,61 @@
 package fr.edf.jenkins.plugins.windows.connector
 
+import static com.cloudbees.plugins.credentials.CredentialsMatchers.anyOf
+import static com.cloudbees.plugins.credentials.CredentialsMatchers.instanceOf
+import static com.cloudbees.plugins.credentials.domains.URIRequirementBuilder.fromUri
+
 import java.time.Instant
 
-import javax.net.ssl.SSLContext
-import javax.net.ssl.X509TrustManager
-
 import org.apache.commons.lang.exception.ExceptionUtils
-import org.apache.http.client.HttpClient
-import org.apache.http.impl.client.CloseableHttpClient
-import org.apache.http.impl.client.HttpClientBuilder
 import org.jenkinsci.Symbol
+import org.jenkinsci.plugins.plaincredentials.StringCredentials
+import org.kohsuke.stapler.AncestorInPath
 import org.kohsuke.stapler.DataBoundConstructor
+import org.kohsuke.stapler.DataBoundSetter
+import org.kohsuke.stapler.QueryParameter
+import org.kohsuke.stapler.verb.POST
+
+import com.cloudbees.plugins.credentials.CredentialsProvider
+import com.cloudbees.plugins.credentials.common.StandardCredentials
+import com.cloudbees.plugins.credentials.common.StandardListBoxModel
 
 import fr.edf.jenkins.plugins.windows.WindowsHost
 import fr.edf.jenkins.plugins.windows.WindowsUser
 import fr.edf.jenkins.plugins.windows.agent.WindowsComputer
 import fr.edf.jenkins.plugins.windows.http.MicroserviceHttpClient
+import fr.edf.jenkins.plugins.windows.util.FormUtils
 import fr.edf.jenkins.plugins.windows.winrm.WinRMCommandException
 import hudson.Extension
 import hudson.model.Descriptor
+import hudson.model.Item
 import hudson.model.TaskListener
+import hudson.security.ACL
 import hudson.slaves.ComputerLauncher
 import hudson.slaves.JNLPLauncher
 import hudson.slaves.SlaveComputer
+import hudson.util.ListBoxModel
+import jenkins.model.Jenkins
 
 class MicroServiceJNLPConnector extends WindowsComputerConnector {
 
     private MicroserviceHttpClient client
+    private String contextPath
 
     @DataBoundConstructor
     MicroServiceJNLPConnector(Boolean useHttps, Boolean disableCertificateCheck, Integer port,
     String credentialsId, Integer connectionTimeout, Integer readTimeout,
-    Integer agentConnectionTimeout, String jenkinsUrl) {
+    Integer agentConnectionTimeout, String jenkinsUrl, contextPath) {
         super(jenkinsUrl, port, useHttps, disableCertificateCheck, credentialsId, connectionTimeout, readTimeout, agentConnectionTimeout)
+        this.contextPath = contextPath
+    }
+
+    String getContextPath() {
+        return contextPath
+    }
+
+    @DataBoundSetter
+    void setContextPath(String contextPath) {
+        this.contextPath = contextPath
     }
 
     @Override
@@ -56,7 +79,35 @@ class MicroServiceJNLPConnector extends WindowsComputerConnector {
     }
 
     @Extension @Symbol("microservice")
-    static final class DescriptorImpl extends Descriptor<WindowsComputerConnector>{
+    static final class DescriptorImpl extends Descriptor<WindowsComputerConnector> {
+
+        /**
+         * List the available credentials
+         * @param host
+         * @param credentialsId
+         * @param item
+         * @return CredentialsId
+         */
+        @POST
+        ListBoxModel doFillCredentialsIdItems(@QueryParameter String host, @QueryParameter String credentialsId,
+                @AncestorInPath Item item) {
+            StandardListBoxModel result = new StandardListBoxModel()
+            boolean notAdmin = item == null && !Jenkins.get().hasPermission(Jenkins.ADMINISTER)
+            boolean noCredentials = item != null && !item.hasPermission(Item.EXTENDED_READ) &&
+                    !item.hasPermission(CredentialsProvider.USE_ITEM)
+
+            if(notAdmin || noCredentials) {
+                return result.includeCurrentValue(credentialsId)
+            }
+            return result
+                    .includeEmptyValue()
+                    .includeMatchingAs(ACL.SYSTEM,
+                    item ?: Jenkins.get(),
+                    StandardCredentials.class,
+                    fromUri(FormUtils.getUri(host).toString()).build(),
+                    anyOf(instanceOf(StringCredentials)))
+                    .includeCurrentValue(credentialsId)
+        }
 
         /**
          * {@inheritDoc}
@@ -71,6 +122,7 @@ class MicroServiceJNLPConnector extends WindowsComputerConnector {
         WindowsHost host
         WindowsUser user
         String jenkinsUrl
+        MicroServiceJNLPConnector microServiceJNLPConnector
         boolean launched
 
         MicroServiceJNLPLauncher(WindowsHost host, WindowsUser user, String jenkinsUrl) {
@@ -115,7 +167,7 @@ class MicroServiceJNLPConnector extends WindowsComputerConnector {
                 if (windowsComputer.isOnline()) {
                     break
                 }
-                if((Instant.now().toEpochMilli() - currentTimestamp) > host.agentConnectionTimeout.multiply(1000).intValue()) {
+                if((Instant.now().toEpochMilli() - currentTimestamp) > microServiceJNLPConnector.agentConnectionTimeout.multiply(1000).intValue()) {
                     launched = false
                     String message = toString().format("Connection timeout for the computer %s", computer.name)
                     listener.error(message)
